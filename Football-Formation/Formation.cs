a543ab8bc06b42f606e638f39fff4da3752c4e11
BaBuIIn <vddycfuhhf1055@gmail.com>
2025-05-17 19:54:31 +0300

    Created class Formation

---
+++ Football-Formation/Formation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Football_Formation
+{
+    public enum FormationType
+    {
+        //4-4-2
+        FourFourTwo,
+        //4-3-3
+        FourThreeThree,
+        //4-2-3-1
+        FourTwoThreeOne
+    }
+    public class Formation
+    {
+        private List<Player> Players;
+        private FormationType FormationName;
+
+        public Formation(FormationType formationName)
+        {
+            Players = new List<Player>();
+            FormationName = formationName;
+        }
+    }
+}
